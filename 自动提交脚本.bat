48a32fe4bccb51bc3ae08c467038677bb9206a56
CNdragoner <1290862616@qq.com>
2021-07-28 18:26:00 +0800

    $msg

--- 自动提交脚本.bat
+++ 自动提交脚本.bat
@@ -1,9 +1,15 @@
-time=$(date "+%Y-%m-%d")
-echo "commit success!，your commit msg is $time , welcome to my program"
+echo -n "Enter your commit message:"        #参数-n的作用是不换行，echo默认是换行
+read  msg                                   #从键盘获取信息输入
+echo "commit success!，your commit msg is $msg , welcome to my program"     
 
 git add .
 
-git commit -m "$time"
+git commit -m "$msg"
 
+# 上传到 gitee
+git push origin master
 # 上传到 github
-git push -u origin main
+git push origin_github master
+
+# 不关闭当前窗口
+exec /bin/bash

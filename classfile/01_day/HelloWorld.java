16c0183feae0ee8c9a4f285767364d5f2d5374d6
CNdragoner <1290862616@qq.com>
2021-08-07 18:18:52 +0800

    21/8/7_01-day

---
+++ classfile/01_day/HelloWorld.java
@@ -0,0 +1,15 @@
+//public表示公开的
+//class表示定义一个类
+//HelloWorld表示一个类名
+public class HelloWorld{
+	/*
+		static表示静态的
+		void表示空
+		main表示方法名是main，是程序的主入口
+		(String[] args)是一个main方法形式参数列表
+		
+	*/
+	public static void main(String[] args){
+		System.out.println("HelloWorld!");
+	}
+}
